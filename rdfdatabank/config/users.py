bda3c5a724a34f2fd0841c8eda1720c4bea6618b
anusharanganathan <anusha.ranganathan@ieee.org>
2010-06-22 14:21:23 +0100

    Adding users.py with the user data in config

---
+++ rdfdatabank/config/users.py
@@ -0,0 +1,5 @@
+_USERS = {
+    'admin': {'first_name':'anusha', 'last_name':'Ranganathan', 'owner':'*', 'role':'admin'},
+    'admiral': {'name':'ADMIRAL Project', 'description':'ADMIRAL: A Data Management Infrastructure for Research', 'owner':['admiral'], 'role':'user'},
+    'eidcsr': {'name':'EIDCSR Project', 'description':'The Embedding Institutional Data Curation Services in Research (EIDCSR) project is addressing the research data management and curation challenges of three research groups in the University of Oxford.', 'owner':['eidcsr'], 'role':'user'},
+    }
