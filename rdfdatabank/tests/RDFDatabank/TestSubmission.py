9d5e1f990cf9ddb96b68b8355517d67f7caa1d3b
Anusha Ranganathan <anusha.ranganathan@ieee.org>
2010-11-16 09:53:31 +0000

    Extended tests testFileUnpack, testSymlinkFileUnpack, testFileUploadToUnpackedDataset and testUpdateUnpackedDataset with state information

--- rdfdatabank/tests/RDFDatabank/TestSubmission.py
+++ rdfdatabank/tests/RDFDatabank/TestSubmission.py
@@ -321,7 +321,7 @@ class TestSubmission(SparqlQueryTestCase.SparqlQueryTestCase):
             ,("TestSubmission.9", 403, "Forbidden")
             ,("""Test"Submission""", 403, "Forbidden")
             ,("Test'Submission", 403, "Forbidden")
-            #,("Test Submission", 403, "Forbidden")
+            #,("Test Submission", 403, "Forbidden") #This is being posted as 'Test' - Why?
             ,("TestSubmission$", 403, "Forbidden")
         ]
         fields = []
@@ -503,7 +503,7 @@ class TestSubmission(SparqlQueryTestCase.SparqlQueryTestCase):
         state = data['state']
         parts = data['parts']
         self.assertEqual(state['item_id'], "TestSubmission", "Submission item identifier")
-        self.assertEqual(len(state['versions']), 3, "Two versions")
+        self.assertEqual(len(state['versions']), 3, "Three versions")
         self.assertEqual(state['versions'][0], '1', "Version 1")
         self.assertEqual(state['versions'][1], '2', "Version 2")
         self.assertEqual(state['versions'][2], '3', "Version 3")
@@ -663,7 +663,7 @@ class TestSubmission(SparqlQueryTestCase.SparqlQueryTestCase):
         state = data['state']
         parts = data['parts']
         self.assertEqual(state['item_id'], "TestSubmission", "Submission item identifier")
-        self.assertEqual(len(state['versions']), 3, "Two versions")
+        self.assertEqual(len(state['versions']), 3, "Three versions")
         self.assertEqual(state['versions'][0], '1', "Version 1")
         self.assertEqual(state['versions'][1], '2', "Version 2")
         self.assertEqual(state['versions'][2], '3', "Version 3")
@@ -920,7 +920,7 @@ class TestSubmission(SparqlQueryTestCase.SparqlQueryTestCase):
         state = data['state']
         parts = data['parts']
         self.assertEqual(state['item_id'], "TestSubmission", "Submission item identifier")
-        self.assertEqual(len(state['versions']), 3, "Two versions")
+        self.assertEqual(len(state['versions']), 3, "Three versions")
         self.assertEqual(state['versions'][0], '1', "Version 1")
         self.assertEqual(state['versions'][1], '2', "Version 2")
         self.assertEqual(state['versions'][2], '3', "Version 3")
@@ -1027,7 +1027,31 @@ class TestSubmission(SparqlQueryTestCase.SparqlQueryTestCase):
             expect_status=200, expect_reason="OK", expect_type="*/*")
         checkdata = open("data/testdir/directory/file1.b").read()
         self.assertEqual(filedata, checkdata, "Difference between local and remote data!")
-        #TODO: Access state information
+        #Access state information of TestSubmission-testdir
+        data = self.doHTTP_GET(
+            resource="states/TestSubmission-testdir", 
+            expect_status=200, expect_reason="OK", expect_type="application/json")
+        state = data['state']
+        parts = data['parts']
+        self.assertEqual(state['item_id'], "TestSubmission-testdir", "Submission item identifier")
+        self.assertEqual(len(state['versions']), 2, "Two versions")
+        self.assertEqual(state['versions'][0], '1', "Version 1")
+        self.assertEqual(state['versions'][1], '2', "Version 2")
+        self.assertEqual(state['currentversion'], '2', "Current version == 2")
+        self.assertEqual(state['rdffileformat'], 'xml', "RDF file type")
+        self.assertEqual(state['rdffilename'], 'manifest.rdf', "RDF file name")
+        self.assertEqual(state['files']['1'], ['manifest.rdf'], "List should contain just manifest.rdf")
+        self.assertEqual(state['files']['2'], ['manifest.rdf'], "List should contain just manifest.rdf")
+        self.assertEqual(len(state['metadata_files']['1']), 0, "metadata_files of version 1")
+        self.assertEqual(len(state['metadata_files']['2']), 0, "metadata_files of version 2")
+        self.assertEqual(len(state['subdir']['1']), 0,   "Subdirectory count for version 1")
+        self.assertEqual(len(state['subdir']['2']), 0,   "Subdirectory count for version 2")
+        self.assertEqual(state['metadata']['createdby'], RDFDatabankConfig.endpointuser, "Created by")
+        self.assertEqual(state['metadata']['embargoed'], True, "Embargoed?")
+        self.assertEqual(len(parts.keys()), 3, "Parts")
+        self.assertEqual(len(parts['4=TestSubmission'].keys()), 13, "File stats for 4=TestSubmission")
+        self.assertEqual(len(parts['manifest.rdf'].keys()), 13, "File stats for manifest.rdf")
+        
         # Delete the dataset TestSubmission-testdir
         self.doHTTP_DELETE(
             resource="datasets/TestSubmission-testdir", 
@@ -1112,6 +1136,30 @@ class TestSubmission(SparqlQueryTestCase.SparqlQueryTestCase):
             expect_status=200, expect_reason="OK", expect_type="*/*")
         checkdata = open("data/testdir/directory/file1.b").read()
         self.assertEqual(filedata, checkdata, "Difference between local and remote data!")
+        #Access state information of TestSubmission-testdir
+        data = self.doHTTP_GET(
+            resource="states/TestSubmission-testdir", 
+            expect_status=200, expect_reason="OK", expect_type="application/json")
+        state = data['state']
+        parts = data['parts']
+        self.assertEqual(state['item_id'], "TestSubmission-testdir", "Submission item identifier")
+        self.assertEqual(len(state['versions']), 2, "Two versions")
+        self.assertEqual(state['versions'][0], '1', "Version 1")
+        self.assertEqual(state['versions'][1], '2', "Version 2")
+        self.assertEqual(state['currentversion'], '2', "Current version == 2")
+        self.assertEqual(state['rdffileformat'], 'xml', "RDF file type")
+        self.assertEqual(state['rdffilename'], 'manifest.rdf', "RDF file name")
+        self.assertEqual(state['files']['1'], ['manifest.rdf'], "List should contain just manifest.rdf")
+        self.assertEqual(state['files']['2'], ['manifest.rdf'], "List should contain just manifest.rdf")
+        self.assertEqual(len(state['metadata_files']['1']), 0, "metadata_files of version 1")
+        self.assertEqual(len(state['metadata_files']['2']), 0, "metadata_files of version 2")
+        self.assertEqual(len(state['subdir']['1']), 0,   "Subdirectory count for version 1")
+        self.assertEqual(len(state['subdir']['2']), 0,   "Subdirectory count for version 2")
+        self.assertEqual(state['metadata']['createdby'], RDFDatabankConfig.endpointuser, "Created by")
+        self.assertEqual(state['metadata']['embargoed'], True, "Embargoed?")
+        self.assertEqual(len(parts.keys()), 3, "Parts")
+        self.assertEqual(len(parts['4=TestSubmission'].keys()), 13, "File stats for 4=TestSubmission")
+        self.assertEqual(len(parts['manifest.rdf'].keys()), 13, "File stats for manifest.rdf")
         # Delete the dataset TestSubmission-testdir
         self.doHTTP_DELETE(
             resource="datasets/TestSubmission-testdir", 
@@ -1183,7 +1231,7 @@ class TestSubmission(SparqlQueryTestCase.SparqlQueryTestCase):
             reqdata, reqtype, 
             resource="datasets/TestSubmission-testdir/", 
             expect_status=201, expect_reason="Created")
-        # Access dataset TestSub,ission-testdir, check response
+        # Access dataset TestSubmission-testdir, check response
         rdfdata = self.doHTTP_GET(
             resource="datasets/TestSubmission-testdir",  
             expect_status=200, expect_reason="OK", expect_type="application/rdf+xml")
@@ -1210,7 +1258,34 @@ class TestSubmission(SparqlQueryTestCase.SparqlQueryTestCase):
         self.failUnless((subj,URIRef(ore+"aggregates"),URIRef(base+"testdir/test-csv.csv")) in rdfgraph)
         self.failUnless((subj,URIRef(ore+"aggregates"),URIRef(base+"testdir2.zip")) in rdfgraph)
         self.failUnless((subj,URIRef(oxds+"currentVersion"),"3") in rdfgraph, 'oxds:currentVersion')
-        #TODO: State information
+        #Access state information of TestSubmission-testdir
+        data = self.doHTTP_GET(
+            resource="states/TestSubmission-testdir", 
+            expect_status=200, expect_reason="OK", expect_type="application/json")
+        state = data['state']
+        parts = data['parts']
+        self.assertEqual(state['item_id'], "TestSubmission-testdir", "Submission item identifier")
+        self.assertEqual(len(state['versions']), 3, "Three versions")
+        self.assertEqual(state['versions'][0], '1', "Version 1")
+        self.assertEqual(state['versions'][1], '2', "Version 2")
+        self.assertEqual(state['versions'][2], '3', "Version 3")
+        self.assertEqual(state['currentversion'], '3', "Current version == 3")
+        self.assertEqual(state['rdffileformat'], 'xml', "RDF file type")
+        self.assertEqual(state['rdffilename'], 'manifest.rdf', "RDF file name")
+        self.assertEqual(state['files']['1'], ['manifest.rdf'], "List should contain just manifest.rdf")
+        self.assertEqual(state['files']['2'], ['manifest.rdf'], "List should contain just manifest.rdf")
+        self.assertEqual(state['files']['3'], ['manifest.rdf'], "List should contain just manifest.rdf")
+        self.assertEqual(len(state['metadata_files']['1']), 0, "metadata_files of version 1")
+        self.assertEqual(len(state['metadata_files']['2']), 0, "metadata_files of version 2")
+        self.assertEqual(len(state['metadata_files']['3']), 0, "metadata_files of version 3")
+        self.assertEqual(len(state['subdir']['1']), 0,   "Subdirectory count for version 1")
+        self.assertEqual(len(state['subdir']['2']), 0,   "Subdirectory count for version 2")
+        self.assertEqual(len(state['subdir']['3']), 0,   "Subdirectory count for version 3")
+        self.assertEqual(state['metadata']['createdby'], RDFDatabankConfig.endpointuser, "Created by")
+        self.assertEqual(state['metadata']['embargoed'], True, "Embargoed?")
+        self.assertEqual(len(parts.keys()), 3, "Parts")
+        self.assertEqual(len(parts['4=TestSubmission'].keys()), 13, "File stats for 4=TestSubmission")
+        self.assertEqual(len(parts['manifest.rdf'].keys()), 13, "File stats for manifest.rdf")
         # Delete the dataset TestSubmission-testdir
         self.doHTTP_DELETE(
             resource="datasets/TestSubmission-testdir", 
@@ -1342,6 +1417,34 @@ class TestSubmission(SparqlQueryTestCase.SparqlQueryTestCase):
         self.failUnless((subj,URIRef(ore+"aggregates"),URIRef(base+"testdir2/directory/file2.a")) in rdfgraph)
         self.failUnless((subj,URIRef(ore+"aggregates"),URIRef(base+"testdir2/test-csv.csv")) in rdfgraph)
         self.failUnless((subj,URIRef(oxds+"currentVersion"),"3") in rdfgraph, 'oxds:currentVersion')
+        #Access state information of TestSubmission-testdir
+        data = self.doHTTP_GET(
+            resource="states/TestSubmission-testdir", 
+            expect_status=200, expect_reason="OK", expect_type="application/json")
+        state = data['state']
+        parts = data['parts']
+        self.assertEqual(state['item_id'], "TestSubmission-testdir", "Submission item identifier")
+        self.assertEqual(len(state['versions']), 3, "Three versions")
+        self.assertEqual(state['versions'][0], '1', "Version 1")
+        self.assertEqual(state['versions'][1], '2', "Version 2")
+        self.assertEqual(state['versions'][2], '3', "Version 3")
+        self.assertEqual(state['currentversion'], '3', "Current version == 3")
+        self.assertEqual(state['rdffileformat'], 'xml', "RDF file type")
+        self.assertEqual(state['rdffilename'], 'manifest.rdf', "RDF file name")
+        self.assertEqual(state['files']['1'], ['manifest.rdf'], "List should contain just manifest.rdf")
+        self.assertEqual(state['files']['2'], ['manifest.rdf'], "List should contain just manifest.rdf")
+        self.assertEqual(state['files']['3'], ['manifest.rdf'], "List should contain just manifest.rdf")
+        self.assertEqual(len(state['metadata_files']['1']), 0, "metadata_files of version 1")
+        self.assertEqual(len(state['metadata_files']['2']), 0, "metadata_files of version 2")
+        self.assertEqual(len(state['metadata_files']['3']), 0, "metadata_files of version 3")
+        self.assertEqual(len(state['subdir']['1']), 0,   "Subdirectory count for version 1")
+        self.assertEqual(len(state['subdir']['2']), 0,   "Subdirectory count for version 2")
+        self.assertEqual(len(state['subdir']['3']), 0,   "Subdirectory count for version 3")
+        self.assertEqual(state['metadata']['createdby'], RDFDatabankConfig.endpointuser, "Created by")
+        self.assertEqual(state['metadata']['embargoed'], True, "Embargoed?")
+        self.assertEqual(len(parts.keys()), 3, "Parts")
+        self.assertEqual(len(parts['4=TestSubmission'].keys()), 13, "File stats for 4=TestSubmission")
+        self.assertEqual(len(parts['manifest.rdf'].keys()), 13, "File stats for manifest.rdf")
         # Delete the dataset TestSubmission-testdir
         self.doHTTP_DELETE(
             resource="datasets/TestSubmission-testdir", 
