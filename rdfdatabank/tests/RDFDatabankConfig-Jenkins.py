7f972a93f793d67dfdc485bfc3b74acd66797f66
Databank Admin <databankadmin@databank.localdomain>
2011-07-01 13:27:27 +0100

    Added the ini files and requirements.txt needed for Jenkins along with a basic password and users.py file

---
+++ rdfdatabank/tests/RDFDatabankConfig-Jenkins.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# $Id: $
+"""
+Define configuration for RDFDatabank testing
+
+$Rev: $
+"""
+
+class RDFDatabankConfig:
+
+    granary_uri_root="http://dataflow-jenkins.bodleian.ox.ac.uk/"
+
+    # Access via IP address
+    #endpointhost="databank.ora.ox.ac.uk"
+    #Access to local dev VM
+    endpointhost="dataflow-jenkins.bodleian.ox.ac.uk"
+ 
+    # Access credentials for testing
+    endpointpath="/sandbox/"
+    
+    # Access credentials for testing from local dev VM as admin
+    endpointuser="admin"
+    endpointpass="test"
+
+    # Access credentials for testing from local dev VM as admin
+    endpointuser="sandbox_user"
+    endpointpass="sandbox"
+
+    # Later, may define methods to override these defaults, e.g. from a configuration file
+
+# End.
