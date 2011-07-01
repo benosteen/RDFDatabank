0cb2552248f3e32bc11509bb2e81fa765305c7ef
Anusha Ranganathan <anusha.ranganathan@ieee.org>
2011-07-01 16:42:46 +0100

    Added a default file for password and users.py which I ca then copy for testing Jenkins

---
+++ rdfdatabank/config/users-default.py
@@ -0,0 +1,1 @@
+_USERS = {'admin': {'owner': '*', 'first_name': 'Databank', 'last_name': 'Admin', 'role': 'admin'}, 'sandbox_user': {'owner': ['sandbox'], 'role': 'user', 'name': 'Sandbox user', 'description': 'User for silo Sandbox'}}
